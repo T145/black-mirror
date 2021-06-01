e89282b8e0684d60470ba6052be2ef62ca8ebdfe
T145 <T145@protonmail.com>
2021-05-31 22:40:22 -0400

    Removed the comment wall

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,24 +9,6 @@ trap 'rm "$the_blacklist" && rm -rf "$downloads"' EXIT || exit 1
 rm -rf hosts
 mkdir hosts -p -m 777
 
-# #
-# ---
-# The gaming list blocks virtually all gaming-related hosts,
-# and the basic oisd list is included in the full version.
-#
-# https://github.com/StevenBlack/hosts#sources-of-hosts-data-unified-in-this-variant
-# ---
-# Now using the complete StevenBlack list.
-# Includes adaway, adguard tracking, whocares, winhelp, and yoyo.
-#
-# https://github.com/EnergizedProtection/block#package-sources
-# ---
-# Now using the complete EnergizedProtection list.
-# Includes adguard, bitcoin, disconnect, reg_cn, reg_cz, reg_de, reg_es, reg_fr,
-# reg_it, reg_nl, reg_ro, reg_ru, reg_vn, stopforumspam, spam404, and winspy.
-# All Anudeep lists are included except the Facebook list, so that's being added in.
-# #
-
 cat sources.json | jq -r 'to_entries[] | [.key, .value.url] | @tsv' |
     awk -F'\t' '{ if ($2 ~ /\.tar.gz$/) { printf "%s\n out=%s.tar.gz\n",$2,$1 } else { printf "%s\n out=%s.txt\n",$2,$1 } }' |
     aria2c -i- -q -d "$downloads" --max-concurrent-downloads=10 --optimize-concurrent-downloads=true --auto-file-renaming=false --realtime-chunk-checksum=false --async-dns-server=[1.1.1.1:53,1.0.0.1:53,8.8.8.8:53,8.8.4.4:53,9.9.9.9:53,9.9.9.10:53,77.88.8.8:53,77.88.8.1:53,208.67.222.222:53,208.67.220.220:53]
