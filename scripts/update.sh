ef84178c2ff3504db3a03869d6661d04342fcb90
T145 <T145@protonmail.com>
2021-06-01 16:08:37 -0400

    url var no longer needed

--- scripts/update.sh
+++ scripts/update.sh
@@ -13,8 +13,8 @@ cat sources.json | jq -r 'to_entries[] | [.key, .value.url] | @tsv' |
     awk -F'\t' '{ if ($2 ~ /\.tar.gz$/) { printf "%s\n out=%s.tar.gz\n",$2,$1 } else { printf "%s\n out=%s.txt\n",$2,$1 } }' |
     aria2c -i- -q -d "$downloads" --max-concurrent-downloads=10 --optimize-concurrent-downloads=true --auto-file-renaming=false --realtime-chunk-checksum=false --async-dns-server=[1.1.1.1:53,1.0.0.1:53,8.8.8.8:53,8.8.4.4:53,9.9.9.9:53,9.9.9.10:53,77.88.8.8:53,77.88.8.1:53,208.67.222.222:53,208.67.220.220:53]
 
-cat sources.json | jq -r 'to_entries[] | [.key, .value.url, .value.rule] | @tsv' |
-    while IFS=$'\t' read key url rule; do
+cat sources.json | jq -r 'to_entries[] | [.key, .value.rule] | @tsv' |
+    while IFS=$'\t' read key rule; do
         fpath=$(find -P -O3 "$downloads" -type f -name "$key*")
 
         if test -f "whitelists/$key.txt"; then
