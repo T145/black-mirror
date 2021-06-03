14f75718cda23d7eb1a2ad8ae7dfb4821e9f855d
T145 <T145@protonmail.com>
2021-06-03 14:36:38 -0400

    The Ballad of a Retard

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eu #x
 
 downloads=$(mktemp -d)
 trap 'rm -rf "$downloads"' EXIT || exit 1
 
-cat sources.json | jq -S -r 'to_entries[] | [.key, .value.url] | @tsv' |
+jq -r 'to_entries[] | [.key, .value.url] | @tsv' sources.json |
     gawk -F'\t' '{ if ($2 ~ /\.tar.gz$/ || /\.zip$/) { printf "%s\n out=%s.%s\n",$2,$1,gensub(/^(.*[/])?[^.]*[.]?/, "", 1, $2) } else { printf "%s\n out=%s.txt\n",$2,$1 } }' |
     aria2c -i- -q -d "$downloads" --max-concurrent-downloads=10 --optimize-concurrent-downloads=true --auto-file-renaming=false --realtime-chunk-checksum=false --async-dns-server=[1.1.1.1:53,1.0.0.1:53,8.8.8.8:53,8.8.4.4:53,9.9.9.9:53,9.9.9.10:53,77.88.8.8:53,77.88.8.1:53,208.67.222.222:53,208.67.220.220:53]
 
-cat sources.json | jq -S -r 'to_entries[] | [.key, .value.rule] | @tsv' |
-    while IFS=$'\t' read key rule; do
+jq -r 'keys[] as $k | "\($k)#\(.[$k] | .rule)"' sources.json |
+    while IFS=$'#' read key rule; do
         fpath=$(find -P -O3 "$downloads" -type f -name "$key*")
 
         if test -f "whitelists/$key.txt"; then
