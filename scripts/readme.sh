a2fcd46523131c78a7bd2dd3a5f6819a4456cec0
T145 <T145@protonmail.com>
2021-06-10 01:37:08 -0400

    Completed inplace README updates

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -11,6 +11,11 @@ ipv4_size=$(du -h black_ipv4.txt | gawk -F'\t' '{ print $1 }')
 ipv6_entries=$(LC_ALL=en_US.UTF-8 gawk num="$(($(cat black_ipv6.txt | wc -l) - $domain_entries))" -f ./scripts/thousands.awk)
 ipv6_size=$(du -h black_ipv6.txt | gawk -F'\t' '{ print $1 }')
 
-sed -e "s/__DOMAIN_ENTRIES/$domain_entries/g" -e "s/__DOMAIN_SIZE/${domain_size}B/g" \
-    -e "s/__IPV4_ENTRIES/$ipv4_entries/g" -e "s/__IPV4_SIZE/${ipv4_size}B/g" \
-    -e "s/__IPV6_ENTRIES/$ipv6_entries/g" -e "s/__IPV6_SIZE/${ipv6_size}B/g" ./.github/README.md >|./.github/README.md
+sed -i \
+    -e "s/\(<td id=\"domain-entries\">\).*\(<\/td>\)/<td id=\"domain-entries\">$domain_entries<\/td>/g" \
+    -e "s/\(<td id=\"domain-size\">\).*\(<\/td>\)/<td id=\"domain-size\">${domain_size}B<\/td>/g" \
+    -e "s/\(<td id=\"ipv4-entries\">\).*\(<\/td>\)/<td id=\"ipv4-entries\">$ipv4_entries<\/td>/g" \
+    -e "s/\(<td id=\"ipv4-size\">\).*\(<\/td>\)/<td id=\"ipv4-size\">${ipv4_size}B<\/td>/g" \
+    -e "s/\(<td id=\"ipv6-entries\">\).*\(<\/td>\)/<td id=\"ipv6-entries\">$ipv6_entries<\/td>/g" \
+    -e "s/\(<td id=\"ipv6-size\">\).*\(<\/td>\)/<td id=\"ipv6-size\">${ipv6_size}B<\/td>/g" ./.github/README.md
+# xmlstarlet is probably better to handle this
