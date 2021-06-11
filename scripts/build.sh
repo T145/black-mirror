5e86a7671b33a5fdaa13b65320469ecd7a0eb2e5
T145 <T145@protonmail.com>
2021-06-10 20:15:00 -0400

    Applied shellcheck fixes and the proper bash debug mode

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-set -eu
+set -euo pipefail
 
 downloads=$(mktemp -d)
 trap 'rm -rf "$downloads"' EXIT || exit 1
@@ -9,14 +8,14 @@ for color in 'white' 'black'; do
     jq --arg color "$color" 'to_entries[] | select(.value.color == $color)' sources.json |
         jq -r -s 'from_entries | keys[] as $k | "\($k)#\(.[$k] | .mirrors)"' |
         while IFS=$'#' read -r key mirrors; do
-            echo $mirrors | tr -d '[]"' | tr -s ',' "\t" | gawk -v key="$key" '{
+            echo "$mirrors" | tr -d '[]"' | tr -s ',' "\t" | gawk -v key="$key" '{
                 if ($0 ~ /\.tar.gz$/ || /\.zip$/) {
                     printf "%s\n out=%s.%s\n",$0,key,gensub(/^(.*[/])?[^.]*[.]?/, "", 1, $0)
                 } else {
                     printf "%s\n out=%s.txt\n",$0,key
                 }
             }'
-        done | aria2c -i- -q -d "${downloads}/${color}" --max-concurrent-downloads=10 --optimize-concurrent-downloads=true --auto-file-renaming=false --realtime-chunk-checksum=false --async-dns-server=[1.1.1.1:53,1.0.0.1:53,8.8.8.8:53,8.8.4.4:53,9.9.9.9:53,9.9.9.10:53,77.88.8.8:53,77.88.8.1:53,208.67.222.222:53,208.67.220.220:53]
+        done | aria2c -i- -q -d "${downloads}/${color}" --max-concurrent-downloads=10 --optimize-concurrent-downloads=true --auto-file-renaming=false --realtime-chunk-checksum=false --async-dns-server=1.1.1.1:53,1.0.0.1:53,8.8.8.8:53,8.8.4.4:53,9.9.9.9:53,9.9.9.10:53,77.88.8.8:53,77.88.8.1:53,208.67.222.222:53,208.67.220.220:53
 
     for format in 'domain' 'ipv4' 'ipv6'; do
         jq --arg color "$color" --arg format "$format" 'to_entries[] | select(.value.color == $color and .value.format == $format)' sources.json |
@@ -28,10 +27,10 @@ for color in 'white' 'black'; do
                 *.tar.gz)
                     # Both Shallalist and Ut-capitole adhere to this format
                     # If any archives are added that do not, this line needs to change
-                    tar -xOzf $fpath --wildcards-match-slash --wildcards '*/domains'
+                    tar -xOzf "$fpath" --wildcards-match-slash --wildcards '*/domains'
                     ;;
-                *.zip) zcat $fpath ;;
-                *) cat $fpath ;;
+                *.zip) zcat "$fpath" ;;
+                *) cat "$fpath" ;;
                 esac |
                     gawk --sandbox -O -- "$rule" | # apply the regex rule
                     gawk '!x[$0]++' |              # filter duplicates out
