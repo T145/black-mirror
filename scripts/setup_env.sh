bbe82bfab95e25590c5be52fff1df4b835d1e8cb
T145 <T145@protonmail.com>
2021-06-11 15:39:39 -0400

    Minor optimizations

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -4,20 +4,15 @@ set -euo pipefail
 install_if_not_exist() {
   if dpkg -s "$1" &>/dev/null; then
     PKG_EXIST=$(dpkg -s "$1" | grep "install ok installed")
-    if [ -z "$PKG_EXIST" ]; then
-      sudo apt install "$1" -y
+    if [[ -n "$PKG_EXIST" ]]; then
+      return
     fi
-  else
-    sudo apt install "$1" -y
   fi
+  sudo apt install "$1" -y
 }
 
 sudo apt update -y
 
-# https://github.com/actions/virtual-environments/blob/main/images/linux/Ubuntu2004-README.md
-install_if_not_exist aria2
-install_if_not_exist coreutils
-install_if_not_exist gawk
-install_if_not_exist jq
-install_if_not_exist moreutils
-install_if_not_exist sed
+for dep in aria2 coreutils gawk jq moreutils sed; do
+  install_if_not_exist $dep
+done
