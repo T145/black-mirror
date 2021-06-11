ae2b2e3a5be99b7620ae0770d9223b41556aa3d0
T145 <T145@protonmail.com>
2021-06-11 11:44:08 -0400

    Some readme update fixes

--- .github/README.md
+++ .github/README.md
@@ -200,8 +200,8 @@
         <td style="text-align:center">black_domain.txt</td>
         <td style="text-align:center">Contains regular host
         entries</td>
-        <td id="domain-count">7,244,324</td>
-        <td id="domain-filesize">161MB</td>
+        <td id="domain_count">7,244,324</td>
+        <td id="domain_filesize">161MB</td>
         <td style="text-align:center">
           <a href=
           "https://github.com/T145/the-blacklist/releases/latest/download/black_domain.tar.gz">
@@ -215,8 +215,8 @@
           "https://github.com/StevenBlack/hosts#we-recommend-using-0000-instead-of-127001">
           <code>0.0.0.0</code></a>
         </td>
-        <td id="ipv4-count">411,745</td>
-        <td id="ipv4-filesize">225MB</td>
+        <td id="ipv4_count">411,745</td>
+        <td id="ipv4_filesize">225MB</td>
         <td style="text-align:center">
           <a href=
           "https://github.com/T145/the-blacklist/releases/latest/download/black_ipv4.tar.gz">
@@ -230,8 +230,8 @@
           "https://stackoverflow.com/questions/40189084/what-is-ipv6-for-localhost-and-0-0-0-0">
           <code>::</code></a>
         </td>
-        <td id="ipv6-count">53,894</td>
-        <td id="ipv6-filesize">182MB</td>
+        <td id="ipv6_count">53,894</td>
+        <td id="ipv6_filesize">182MB</td>
         <td style="text-align:center">
           <a href=
           "https://github.com/T145/the-blacklist/releases/latest/download/black_ipv6.tar.gz">
--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -4,27 +4,30 @@ set -euo pipefail
 export LANG=en_US.UTF-8
 export LANGUAGE=en:el
 
-unique_domain_count=$(cat black_domain.txt | wc -l)
-unique_ipv4_count=$(($(cat black_ipv4.txt | wc -l) - unique_domain_count))
-unique_ipv6_count=$(($(cat black_ipv6.txt | wc -l) - unique_domain_count))
+unique_domain_count=$(wc -l <black_domain.txt)
+unique_ipv4_count=$(($(wc -l <black_ipv4.txt) - unique_domain_count))
+unique_ipv6_count=$(($(wc -l <black_ipv6.txt) - unique_domain_count))
 
 domain_count=$(printf "%'d" "$unique_domain_count")
 ipv4_count=$(printf "%'d" "$unique_ipv4_count")
 ipv6_count=$(printf "%'d" "$unique_ipv6_count")
 
 get_filesize() {
-    du -h "$1" | gawk -F'\t' '{ print $1 }'
+    stat -c %s "$1" | numfmt --to=iec
 }
 
 domain_filesize=$(get_filesize black_domain.txt)
 ipv4_filesize=$(get_filesize black_ipv4.txt)
 ipv6_filesize=$(get_filesize black_ipv6.txt)
 
-sed -i \
-    -e "s/\(<td id=\"domain-count\">\).*\(<\/td>\)/<td id=\"domain-count\">$domain_count<\/td>/g" \
-    -e "s/\(<td id=\"domain-filesize\">\).*\(<\/td>\)/<td id=\"domain-filesize\">${domain_filesize}B<\/td>/g" \
-    -e "s/\(<td id=\"ipv4-count\">\).*\(<\/td>\)/<td id=\"ipv4-count\">$ipv4_count<\/td>/g" \
-    -e "s/\(<td id=\"ipv4-filesize\">\).*\(<\/td>\)/<td id=\"ipv4-filesize\">${ipv4_filesize}B<\/td>/g" \
-    -e "s/\(<td id=\"ipv6-count\">\).*\(<\/td>\)/<td id=\"ipv6-count\">$ipv6_count<\/td>/g" \
-    -e "s/\(<td id=\"ipv6-filesize\">\).*\(<\/td>\)/<td id=\"ipv6-filesize\">${ipv6_filesize}B<\/td>/g" ./.github/README.md
-# xmlstarlet is probably better to handle this
+# Produces a sed script that replaces TD element contents
+# with the value of a same-named variable.
+# Arguments: variable name list
+make_subst_script() {
+    for i; do
+        printf 's/\(<td id="%s">\).*\(<\/td>\)/\\1%s\\2/\n' "$i" "${!i}"
+    done
+}
+
+sed -e "$(make_subst_script domain_count domain_filesize ipv4_count ipv4_filesize ipv6_count ipv6_filesize)" \
+    -i .github/README.md
