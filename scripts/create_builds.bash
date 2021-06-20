bb8906cc4cc1787dd5f8033de5031a0793a07c2f
T145 <T145@protonmail.com>
2021-06-19 21:36:25 -0400

    Added multiformat list processing (Closes #25)

--- scripts/create_builds.bash
+++ scripts/create_builds.bash
@@ -5,13 +5,8 @@ umask 055         # change all generated file perms from 755 to 700
 readonly DOWNLOADS=$(mktemp -d)
 trap 'rm -rf "$DOWNLOADS"' EXIT || exit 1
 
-# params: list name, sort column, cache dir
-sort_list() {
-    sort -o "$1" -k "$2" -u -S 90% --parallel=4 -T "$3" "$1"
-}
-
 for color in 'white' 'black'; do
-    local cache_dir="${DOWNLOADS}/${color}"
+    cache_dir="${DOWNLOADS}/${color}"
 
     jq -r --arg color "$color" 'to_entries[] |
         select(.value.color == $color) |
@@ -20,58 +15,53 @@ for color in 'white' 'black'; do
         (.mirrors | join("\t")), " out=\(.key).\(.extension)"' sources.json |
         aria2c --conf-path='./configs/aria2.conf' -d "$cache_dir"
 
-    for format in 'domain' 'ipv4' 'ipv6'; do
-        local list_name="${color}_${format}.txt"
+    # this logic is technically order agnostic
+    jq -r --arg color "$color" 'to_entries[] | select(.value.color == $color) |
+        .key as $k | .value.filters[] | "\($k)#\(.engine)#\(.format)#\(.rule)"' sources.json |
+        while IFS='#' read -r key engine format rule; do
+            list="${color}_${format}.txt"
+            file_path=$(find -P -O3 "$cache_dir" -type f -name "$key*")
 
-        jq -r --arg color "$color" --arg format "$format" 'to_entries[] |
-            select(.value.color == $color and .value.format == $format) | "\(.key)#\(.value.rule)"' sources.json |
-            while IFS=$'#' read -r key rule; do
-                local fpath=$(find -P -O3 "$cache_dir" -type f -name "$key*")
-
-                if [[ $fpath == *.json ]]; then
-                    jq -r "$rule" "$fpath"
-                else
-                    case $fpath in
-                    *.tar.gz)
-                        # Both Shallalist and Ut-capitole adhere to this format
-                        # If any archives are added that do not, this line needs to change
-                        tar -xOzf "$fpath" --wildcards-match-slash --wildcards '*/domains'
-                        ;;
-                    *.zip) zcat "$fpath" ;;
-                    *) cat "$fpath" ;;
-                    esac | gawk --sandbox -O -- "$rule"
-                fi | gawk -v format="$format" -v filename="$list_name" '
+            case $file_path in
+            *.tar.gz)
+                # Both Shallalist and Ut-capitole adhere to this format
+                # If any archives are added that do not, this line needs to change
+                tar -xOzf "$file_path" --wildcards-match-slash --wildcards '*/domains'
+                ;;
+            *.zip) zcat "$file_path" ;;
+            *) cat "$file_path" ;;
+            esac |
+                case $engine in
+                jq) jq -r "$rule" ;;
+                gawk) gawk --sandbox -O -- "$rule" ;;
+                mawk) mawk "$rule" ;;
+                *) echo "WARN: \"${key}\" doesn't have a valid engine!" ;;
+                esac | gawk -v format="$format" -v list="$list" '
                         BEGIN {
                             prefixes["ipv4"] = "0.0.0.0 "
                             prefixes["ipv6"] = ":: "
                             prefixes["domain"] = ""
                         }
                         !seen[$0]++ {
-                            print prefixes[format] $0 >> filename
+                            print prefixes[format] $0 >> list
                         }'
-            done
+        done
 
-        if test -f "$list_name"; then
-            if [[ "$format" == 'domain' ]]; then
-                sort_list "$list_name" 1 "$cache_dir"
-            else
-                sort_list "$list_name" 2 "$cache_dir"
-            fi
+    for format in 'ipv4' 'ipv6' 'domain'; do
+        list="${color}_${format}.txt"
+
+        if test -f "$list"; then
+            sort -o "$list" -u -S 90% --parallel=4 -T "$cache_dir" "$list"
 
             if [[ "$color" == 'black' ]]; then
-                local blacklist="black_${format}.txt"
-
                 if test -f "white_${format}.txt"; then
-                    grep -Fxvf "white_${format}.txt" "$blacklist" | sponge "$blacklist"
+                    grep -Fxvf "white_${format}.txt" "$list" | sponge "$list"
                 fi
 
                 if [[ "$format" == 'domain' ]]; then
-                    gawk '{ print "0.0.0.0 " $0 }' "$blacklist" >'black_ipv4.txt'
-                    gawk '{ print ":: " $0 }' "$blacklist" >'black_ipv6.txt'
+                    gawk '{ print "0.0.0.0 " $0 }' "$list" >>'black_ipv4.txt'
+                    gawk '{ print ":: " $0 }' "$list" >>'black_ipv6.txt'
                 fi
-
-                tar -czf "black_${format}.tar.gz" "$blacklist"
-                md5sum "black_${format}.tar.gz" >"black_${format}.md5"
             fi
         fi
     done
