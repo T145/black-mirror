05e3bc71f651895e98eda197bc56d3057dc56723
T145 <T145@protonmail.com>
2021-06-30 22:12:05 -0400

    Stop chomping the pipe

--- scripts/idn_to_punycode.pl
+++ scripts/idn_to_punycode.pl
@@ -5,12 +5,9 @@ use open ':std', ':encoding(UTF-8)';
 use Try::Tiny;
 
 foreach $line ( <STDIN> ) {
-   chomp ( $line );
-
    try {
+      chomp ( $line );
       my $a = domain_to_ascii( $line );
       print "$a\n";
-   } catch {
-      #warn "Attempted to generate Punycode domain >255 characters!";
    }
 }
