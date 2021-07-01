6b6360cc3d771e480cc946d30ad555776b53b9c0
T145 <T145@protonmail.com>
2021-06-30 21:45:12 -0400

    Label too long my a-A

--- scripts/idn2punycode.pl
+++ scripts/idn2punycode.pl
@@ -1,12 +1,9 @@
 #!/usr/bin/perl
-
 use Net::IDN::Encode ':all';
 use open ':std', ':encoding(UTF-8)';
-
-# https://www.farsightsecurity.com/blog/txt-record/bulkconvertingIDNs-20180918/
-
 foreach $line ( <STDIN> ) {
    chomp ( $line );
    my $a = domain_to_ascii( $line );
    print "$a\n";
 }
+# https://www.farsightsecurity.com/blog/txt-record/bulkconvertingIDNs-20180918/
