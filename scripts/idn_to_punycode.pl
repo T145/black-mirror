2a5eab7516dce948e9f2fd6f7bc74bb729186256
unknown <T145@protonmail.com>
2021-07-03 13:35:48 -0400

    More perl simplification
    Former-commit-id: 78b8974f07e138b02a299a765672baf701c2f98d

--- scripts/idn_to_punycode.pl
+++ scripts/idn_to_punycode.pl
@@ -1,12 +1,10 @@
-#!/usr/bin/perl -W
+#!/usr/bin/perl -Wn
 use strict;
 use Try::Tiny;
 use Net::IDN::Encode ':all';
 use open ':std', ':encoding(UTF-8)';
 
-while ( <STDIN> ) {
-    try {
-        chomp $_;
-        printf "%s\n",domain_to_ascii $_;
-    }
+try {
+    chomp $_;
+    printf "%s\n",domain_to_ascii $_;
 }
