fd1b472a06330fb78f81df5c12c0745eba7a0f29
unknown <T145@protonmail.com>
2021-07-03 08:21:16 -0400

    Cleaned up the Perl script

--- scripts/idn_to_punycode.pl
+++ scripts/idn_to_punycode.pl
@@ -1,8 +1,5 @@
 #!/usr/bin/perl -W
 use strict;
-# https://perldoc.perl.org/perlrun#-W
-#use warnings;
-
 use Try::Tiny;
 use Net::IDN::Encode ':all';
 use open ':std', ':encoding(UTF-8)';
@@ -10,7 +7,6 @@ use open ':std', ':encoding(UTF-8)';
 while ( <STDIN> ) {
     try {
         chomp $_;
-        printf "%s\n",domain_to_ascii $_
-        #print domain_to_ascii($_),"\n";
+        printf "%s\n",domain_to_ascii $_;
     }
 }
