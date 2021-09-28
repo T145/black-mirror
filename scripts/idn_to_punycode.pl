0eb1b41224847e671326d884ef573b6c4b8e67fc
T145 <T145@protonmail.com>
2021-09-28 00:19:49 -0700

    Made perl script portable

--- scripts/idn_to_punycode.pl
+++ scripts/idn_to_punycode.pl
@@ -1,10 +1,30 @@
-#!/usr/bin/perl -Wn
+#!/usr/bin/env perl
+use feature qw(say);
 use open ':std', ':encoding(UTF-8)';
+use warnings;
 use strict;
 use Try::Tiny;
 use Net::IDN::Encode 'domain_to_ascii';
 
-try {
-    chomp $_;
-    printf "%s\n",domain_to_ascii $_;
+# Written as a modulino: See Chapter 17 in "Mastering Perl". Executes main() if
+#   run as script, otherwise, if the file is imported from the test scripts,
+#   main() is not run.
+main() unless caller;
+
+sub main {
+    while (<>) {
+        my $line = parse_line($_);
+        last if !defined $line;
+        say $line;
+    }
+}
+
+sub parse_line {
+    my ($line) = @_;
+
+    chomp $line;
+    my $result = try {
+        domain_to_ascii($line);
+    };
+    return $result;
 }
