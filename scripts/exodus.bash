879724fa4c7cdf53b9680f14b3e93cec9e16ecff
T145 <T145@protonmail.com>
2021-09-28 16:46:34 -0700

    Cleaned up support scripts

--- scripts/exodus.bash
+++ scripts/exodus.bash
@@ -5,27 +5,30 @@ umask 055         # change all generated file perms from 755 to 700
 # edge cases:
 # "mns\..*\.aliyuncs\.com" (currently being ignored)
 # "mst[0-9]*.is.autonavi.com" / "mt[0-9]*.google.cn" (currently being ignored)
-# has a lot of improper domains:
-# (.code_signature | split("|")[] | rtrimstr(".") | split(".") | reverse | join("."))
-curl -s 'https://etip.exodus-privacy.eu.org/trackers/export' |
+
+# these sections currently have invalid domains:
+# (.network_signature | split ("|")[] | gsub("\\\\"; "") | ltrimstr(".*")),
+# (.code_signature | split("|")[] | rtrimstr(".") | split(".") | reverse | join(".")),
+
+# https://etip.exodus-privacy.eu.org/trackers/export
+curl -s 'https://reports.exodus-privacy.eu.org/api/trackers' |
     jq -r '.trackers[] |
-          (.network_signature | split ("|")[] | gsub("\\\\"; "") | ltrimstr(".*"))
-        , (.website | split("/")[2])
+          (.website | split("/")[2])
       | ltrimstr(".")
     ' |
     gawk '{
         switch ($1) {
         case /^([[:alnum:]_-]{1,63}\.)+[[:alpha:]]+([[:space:]]|$)/:
-            print tolower($1) > "exodus_domains.txt"
+            print tolower($1) > "exports/exodus_domains.txt"
             break
         case /^([0-9]{1,3}\.){3}[0-9]{1,3}+(\/|:|$)/:
             sub(/:.*/, "", $1)
-            print $1 > "exodus_ips.txt"
+            print $1 > "exports/exodus_ips.txt"
             break
         }
     }'
 
-for list in 'exodus_domains.txt' 'exodus_ips.txt'; do
+for list in 'exports/exodus_domains.txt' 'exports/exodus_ips.txt'; do
     sort -o "$list" -u -S 90% --parallel=4 "$list"
 done
 
