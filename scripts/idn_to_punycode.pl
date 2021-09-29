6c643bf4ecdc18f848ac3596af463597b0aba55b
T145 <T145@protonmail.com>
2021-09-28 20:23:04 -0700

    Reverted perl script to check runtime issues
    Former-commit-id: dbe40e67cb23e74c7c46b72e1177cbaa6b5fd7ee

--- scripts/idn_to_punycode.pl
+++ scripts/idn_to_punycode.pl
@@ -1,30 +1,13 @@
 #!/usr/bin/env perl
-use feature qw(say);
 use open ':std', ':encoding(UTF-8)';
 use warnings;
 use strict;
 use Try::Tiny;
 use Net::IDN::Encode 'domain_to_ascii';
 
-# Written as a modulino: See Chapter 17 in "Mastering Perl". Executes main() if
-#   run as script, otherwise, if the file is imported from the test scripts,
-#   main() is not run.
-main() unless caller;
-
-sub main {
-    while (<>) {
-        my $line = parse_line($_);
-        last if !defined $line;
-        say $line;
+while (<>) {
+    try {
+        chomp $_;
+        printf "%s\n",domain_to_ascii $_;
     }
-}
-
-sub parse_line {
-    my ($line) = @_;
-
-    chomp $line;
-    my $result = try {
-        domain_to_ascii($line);
-    };
-    return $result;
 }
