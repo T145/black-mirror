338723ebed46eff2d5f08ed74966baab094e03c7
T145 <T145@protonmail.com>
2021-10-06 15:03:38 -0700

    Added editorconfig
    Former-commit-id: a402f36dabfd8ea6ba73713e990364349bd6303b

--- core/build.bash
+++ core/build.bash
@@ -27,132 +27,132 @@ trap 'rm -rf "$DOWNLOADS"' EXIT || exit 1
 
 # params: src_list
 get_file_contents() {
-    case $1 in
-    *.tar.gz)
-        # Both Shallalist and Ut-capitole adhere to this format
-        # If any archives are added that do not, this line needs to change
-        tar -xOzf "$1" --wildcards-match-slash --wildcards '*/domains'
-        ;;
-    *.gz)
-        file "$1"
-        gzip -cd "$1"
-        ;;
-    *.zip) zcat "$1" ;;
-    *.7z) 7za -y -so e "$1" ;;
-    *) cat -s "$1" ;;
-    esac
+  case $1 in
+  *.tar.gz)
+    # Both Shallalist and Ut-capitole adhere to this format
+    # If any archives are added that do not, this line needs to change
+    tar -xOzf "$1" --wildcards-match-slash --wildcards '*/domains'
+    ;;
+  *.gz)
+    file "$1"
+    gzip -cd "$1"
+    ;;
+  *.zip) zcat "$1" ;;
+  *.7z) 7za -y -so e "$1" ;;
+  *) cat -s "$1" ;;
+  esac
 }
 
 # params: engine, rule
 parse_file_contents() {
-    case $1 in
-    cat) cat -s ;;
-    mawk) mawk "$2" ;;
-    gawk) gawk --sandbox -O -- "$2" ;;
-    jq) jq -r "$2" ;;
-    miller)
-        if [[ $2 =~ ^[0-9]+$ ]]; then
-            mlr --mmap --csv --skip-comments -N cut -f "$2"
-        else
-            mlr --mmap --csv --skip-comments --headerless-csv-output cut -f "$2"
-        fi
-        ;;
-    sed) sed -n "$2" ;;
-    perl)
-        # https://unix.stackexchange.com/a/566565
-        # https://manpages.ubuntu.com/manpages/hirsute/man3/Regexp::Common::net.3pm.html
-        perl -MRegexp::Common=net -nE "$2"
-        ;;
-    saxon) java -cp bin/SaxonHE10-5J/saxon-he-10.5.jar net.sf.saxon.Query -config:configs/saxon.xml -s:/dev/stdin -qs:"$2" ;;
-    *)
-        echo "INVALID ENGINE: ${1}"
-        exit 1
-        ;;
-    esac |
-        mawk '$0!~/^$/{print $0}' | # filter blank lines
-        mawk '!seen[$0]++'          # filter duplicates
+  case $1 in
+  cat) cat -s ;;
+  mawk) mawk "$2" ;;
+  gawk) gawk --sandbox -O -- "$2" ;;
+  jq) jq -r "$2" ;;
+  miller)
+    if [[ $2 =~ ^[0-9]+$ ]]; then
+      mlr --mmap --csv --skip-comments -N cut -f "$2"
+    else
+      mlr --mmap --csv --skip-comments --headerless-csv-output cut -f "$2"
+    fi
+    ;;
+  sed) sed -n "$2" ;;
+  perl)
+    # https://unix.stackexchange.com/a/566565
+    # https://manpages.ubuntu.com/manpages/hirsute/man3/Regexp::Common::net.3pm.html
+    perl -MRegexp::Common=net -nE "$2"
+    ;;
+  saxon) java -cp bin/SaxonHE10-5J/saxon-he-10.5.jar net.sf.saxon.Query -config:configs/saxon.xml -s:/dev/stdin -qs:"$2" ;;
+  *)
+    echo "INVALID ENGINE: ${1}"
+    exit 1
+    ;;
+  esac |
+    mawk '$0!~/^$/{print $0}' | # filter blank lines
+    mawk '!seen[$0]++'          # filter duplicates
 }
 
 # params: format, color
 handle_format_output() {
-    case $1 in
-    domain) ./scripts/idn_to_punycode.pl >>"build/${2}_${1}.txt" ;;
-    ipv4)
-        while IFS= read -r line; do
-            case $line in
-            */*) printf "%s\n" "$line" >>"build/${2}_${1}_cidr.txt" ;;   # cidr block
-            *-*) ipcalc "$line" >>"build/${2}_${1}_cidr.txt" ;;          # deaggregate ip range
-            *.*.*.*) printf "%s\n" "$line" >>"build/${2}_${1}.txt" ;;    # ip address
-            *) echo "WARN: This isn't an IPv4 address: ${line}" ;;       # debug if ips are being processed well
-            esac
-        done
-        ;;
-    ipv6) cat >>"build/${2}_${1}.txt" ;;
-    *)
-        echo "INVALID FORMAT: ${1}"
-        exit 1
-        ;;
-    esac
+  case $1 in
+  domain) ./scripts/idn_to_punycode.pl >>"build/${2}_${1}.txt" ;;
+  ipv4)
+    while IFS= read -r line; do
+      case $line in
+      */*) printf "%s\n" "$line" >>"build/${2}_${1}_cidr.txt" ;; # cidr block
+      *-*) ipcalc "$line" >>"build/${2}_${1}_cidr.txt" ;;        # deaggregate ip range
+      *.*.*.*) printf "%s\n" "$line" >>"build/${2}_${1}.txt" ;;  # ip address
+      *) echo "WARN: This isn't an IPv4 address: ${line}" ;;     # debug if ips are being processed well
+      esac
+    done
+    ;;
+  ipv6) cat >>"build/${2}_${1}.txt" ;;
+  *)
+    echo "INVALID FORMAT: ${1}"
+    exit 1
+    ;;
+  esac
 }
 
 main() {
-    local cache_dir
-    local src_list
-    local list
+  local cache_dir
+  local src_list
+  local list
 
-    # make the build directory if it doesn't exist
-    mkdir -p build/
+  # make the build directory if it doesn't exist
+  mkdir -p build/
 
-    for color in 'white' 'black'; do
-        cache_dir="${DOWNLOADS}/${color}"
+  for color in 'white' 'black'; do
+    cache_dir="${DOWNLOADS}/${color}"
 
-        set +e # temporarily disable strict fail, in case downloads fail
-        jq -r --arg color "$color" 'to_entries[] |
+    set +e # temporarily disable strict fail, in case downloads fail
+    jq -r --arg color "$color" 'to_entries[] |
         select(.value.color == $color) |
         {key, mirrors: .value.mirrors} |
         .extension = (.mirrors[0] | match(".(tar.gz|gz|zip|7z|json)").captures[0].string // "txt") |
         (.mirrors | join("\t")), " out=\(.key).\(.extension)"' core/sources.json |
-            aria2c -i- -d "$cache_dir" --conf-path='./configs/aria2.conf'
-        set -e
+      aria2c -i- -d "$cache_dir" --conf-path='./configs/aria2.conf'
+    set -e
 
-        jq -r --arg color "$color" 'to_entries[] |
+    jq -r --arg color "$color" 'to_entries[] |
         select(.value.color == $color) |
          .key as $k | .value.filters[] | "\($k)#\(.engine)#\(.format)#\(.rule)"' core/sources.json |
-            while IFS='#' read -r key engine format rule; do
-                src_list=$(find -P -O3 "$cache_dir" -type f -name "$key*")
+      while IFS='#' read -r key engine format rule; do
+        src_list=$(find -P -O3 "$cache_dir" -type f -name "$key*")
 
-                if [ -n "$src_list" ]; then
-                    get_file_contents "$src_list" |
-                        parse_file_contents "$engine" "$rule" |
-                        handle_format_output "$format" "$color"
-                fi
-                # else the download failed and src_list is empty
-            done
+        if [ -n "$src_list" ]; then
+          get_file_contents "$src_list" |
+            parse_file_contents "$engine" "$rule" |
+            handle_format_output "$format" "$color"
+        fi
+        # else the download failed and src_list is empty
+      done
 
-        for format in "${FORMATS[@]}"; do
-            list="build/${color}_${format}.txt"
+    for format in "${FORMATS[@]}"; do
+      list="build/${color}_${format}.txt"
 
-            if test -f "$list"; then
-                parsort -u -S 99% --parallel=48 -T "$cache_dir" "$list" | sponge "$list"
+      if test -f "$list"; then
+        parsort -u -S 99% --parallel=48 -T "$cache_dir" "$list" | sponge "$list"
 
-                if [[ "$color" == 'black' ]]; then
-                    if test -f "build/white_${format}.txt"; then
-                        grep -Fxvf "build/white_${format}.txt" "$list" | sponge "$list"
-                    fi
+        if [[ "$color" == 'black' ]]; then
+          if test -f "build/white_${format}.txt"; then
+            grep -Fxvf "build/white_${format}.txt" "$list" | sponge "$list"
+          fi
 
-                    checksums="build/black_${format}.checksums"
+          checksums="build/black_${format}.checksums"
 
-                    cat "${list}" | md5sum >> "${checksums}"
-                    cat "${list}" | b2sum >> "${checksums}"
-                    cat "${list}" | sha1sum >> "${checksums}"
-                    cat "${list}" | sha224sum >> "${checksums}"
-                    cat "${list}" | sha256sum >> "${checksums}"
-                    cat "${list}" | sha384sum >> "${checksums}"
-                    cat "${list}" | sha512sum >> "${checksums}"
-                fi
-            fi
-        done
+          cat "${list}" | md5sum >>"${checksums}"
+          cat "${list}" | b2sum >>"${checksums}"
+          cat "${list}" | sha1sum >>"${checksums}"
+          cat "${list}" | sha224sum >>"${checksums}"
+          cat "${list}" | sha256sum >>"${checksums}"
+          cat "${list}" | sha384sum >>"${checksums}"
+          cat "${list}" | sha512sum >>"${checksums}"
+        fi
+      fi
     done
+  done
 }
 
 # https://github.com/koalaman/shellcheck/wiki/SC2218
--- scripts/update_readme.bash
+++ scripts/update_readme.bash
@@ -7,7 +7,7 @@ unique_ipv4_cidr_count=$(wc -l <build/black_ipv4_cidr.txt)
 unique_ipv6_count=$(wc -l <build/black_ipv6.txt)
 
 add_commas() {
-    echo "$1" | sed -e :x -e 's/\([0-9][0-9]*\)\([0-9][0-9][0-9]\)/\1,\2/' -e 'tx'
+  echo "$1" | sed -e :x -e 's/\([0-9][0-9]*\)\([0-9][0-9][0-9]\)/\1,\2/' -e 'tx'
 }
 
 domain_count=$(add_commas "$unique_domain_count")
@@ -16,7 +16,7 @@ ipv4_cidr_count=$(add_commas "$unique_ipv4_cidr_count")
 ipv6_count=$(add_commas "$unique_ipv6_count")
 
 get_filesize() {
-    stat -c %s "$1" | numfmt --to=iec
+  stat -c %s "$1" | numfmt --to=iec
 }
 
 domain_filesize=$(get_filesize build/black_domain.txt)
@@ -28,10 +28,10 @@ ipv6_filesize=$(get_filesize build/black_ipv6.txt)
 # with the value of a same-named variable.
 # Arguments: variable name list
 make_subst_script() {
-    for i; do
-        printf 's/\(<td id="%s">\).*\(<\/td>\)/\\1%s\\2/\n' "${i//_/-}" "${!i}"
-    done
+  for i; do
+    printf 's/\(<td id="%s">\).*\(<\/td>\)/\\1%s\\2/\n' "${i//_/-}" "${!i}"
+  done
 }
 
 sed -e "$(make_subst_script domain_count domain_filesize ipv4_count ipv4_filesize ipv4_cidr_count ipv4_cidr_filesize ipv6_count ipv6_filesize)" \
-    -i README.md
+  -i README.md
