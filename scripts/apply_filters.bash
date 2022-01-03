6712e707381821d70d3df9e6ef6f9ca42f5b57ff
T145 <T145@protonmail.com>
2022-01-03 17:34:06 -0500

    Implemented V2 list processing for present sources
    Former-commit-id: 261fd34811a4b60c43e79d718e8c1262df7cf1db

---
+++ scripts/apply_filters.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+CONTENT_TYPE=$1
+FORMAT_FILTER=$2
+FORMAT=$3
+METHOD=$4
+LIST=$5
+CACHE=$6
+readonly CONTENT_TYPE FORMAT_FILTER FORMAT METHOD LIST CACHE
+
+#case "$CONTENT_FILTER" in
+#    NONE) cat -s "$LIST" ;;
+#    7Z) 7za -y -so e "$LIST" ;;
+#    ZIP) zcat "$LIST" ;;
+#    SQUIDGUARD) tar -xOzf "$LIST" --wildcards-match-slash --wildcards '*/domains' ;;
+#esac |
+
+# content filters will be applied to lists as soon as they're downloaded
+# this way multiple extract operations per format will not be necessary
+
+cat -s "$LIST" |
+    case "$CONTENT_TYPE" in
+        TEXT)
+            case "$FORMAT_FILTER" in
+                MAWK_WITH_COMMENTS_FIRST_COLUMN) mawk '$0~/^[^#]/{print $1}' ;;
+                MAWK_WITH_COMMENTS_SECOND_COLUMN) mawk '$0~/^[^#]/{print $2}' ;;
+                ADGUARD) mawk '' ;;
+                HOSTS_FILE) ghosts -m -o -p -noheader -stats=false ;;
+                ALIENVAULT) mawk -F# '{print $1}' ;;
+            esac
+        ;;
+        JSON)
+            case "$FORMAT_FILTER" in
+                FEODO_DOMAIN) jq -r '.[] | select(.hostname != null) | .hostname' ;;
+                FEODO_IPV4) jq -r '.[].ip_address' ;;
+            esac
+        ;;
+        CSV)
+            case "$FORMAT_FILTER" in
+                NO_HEADER_SECOND_COLUMN) mlr --mmap --csv --skip-comments -N cut -f 2 ;;
+                URLHAUS_DOMAIN) mlr --mmap --csv --skip-comments -N put -S '$3 =~ "https?://([a-z][^/|^:]+)"; $Domain = "\1"' then cut -f Domain then uniq -a ;;
+                URLHAUS_IPV4) mlr --mmap --csv --skip-comments -N put -S '$3 =~ "https?://([0-9][^/|^:]+)"; $IP = "\1"' then cut -f IP then uniq -a ;;
+            esac
+        ;;
+    esac >> "build/${FORMAT,,}_${METHOD,,}.txt"
