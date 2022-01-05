20cb8154977eecd21d8e39f9808c83347d6aec39
T145 <T145@protonmail.com>
2022-01-05 01:56:03 -0500

    Seemingly fixed domain processing yet again

--- scripts/process_domains.pl
+++ scripts/process_domains.pl
@@ -1,37 +1,25 @@
 #!/usr/bin/env perl
 
-use feature qw(say);
-use open ':std', ':encoding(UTF-8)';
 use warnings;
 use strict;
+use open ':std', ':encoding(UTF-8)';
+use feature 'say';
 use Try::Tiny;
 use Net::IDN::Encode 'domain_to_ascii';
-use Data::Validate::Domain qw(is_domain);
+use Text::Trim 'trim';
+use Data::Validate::Domain 'is_domain';
 
-sub main() {
-    while (<>) {
-        chomp($_);
+while (<>) {
+    chomp($_);
+    trim($_);
 
-        # https://perlmaven.com/fatal-errors-in-external-modules
-        eval {
-            my $domain = try {
-                domain_to_ascii($_);
-            };
+    try {
+        my $line = domain_to_ascii($_);
 
-            last if !defined $domain;
-
-            if (is_domain($domain, {
-                domain_allow_underscore => "true"
-            })) {
-                say($domain);
-            }
-        } or do {
-            my $error = $@ || 'Unknown failure';
-            warn "Could not parse '$_' - $error";
+        if (defined($line) && is_domain($line)) {
+            say($line);
         }
+    } catch {
+        warn "[WARN]: Error processing '$_': $@";
     }
 }
-
-# Written as a modulino: See Chapter 17 in "Mastering Perl". Executes main() if
-#   run as script. Otherwise, if the file is imported, main() is not run.
-main() unless caller;
