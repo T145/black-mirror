2ae8a17969e2f9894da02d784e2b1798ef22dd0d
unknown <T145@protonmail.com>
2022-01-12 12:01:13 -0800

    Certego script now checks for Python 2

--- scripts/import_certego.py
+++ scripts/import_certego.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python3
+import sys
+
+if (sys.version_info < (3, 0)):
+    print(" ################# ERROR ################")
+    print(" ========================================")
+    print("   Invalid Python version detected: "+str(sys.version_info[0])+"."+str(sys.version_info[1]))
+    print(" ========================================")
+    print(" It seems your are still using Python 2 even though it's retired.")
+    print(" For more info please read the following: https://pythonclock.org/")
+    print(" ========================================")
+    print(" Try again: python3 /path/to/"+sys.argv[0])
+    print(" ################# ERROR ################")
+    exit(0)
+
 from ast import literal_eval
 from csv import DictReader
 import twint
 from urllib.parse import urlparse
 from os.path import split
 from os import remove
-import sys
+
 sys.dont_write_bytecode = True
 
 # might be "shadow-banned" for being a bot,
