8727017712128fcb5ff91d4598fb5dfd1ab24efb
T145 <T145@protonmail.com>
2022-05-09 15:54:43 -0700

    Overhauled actions: docs should update properly
    Former-commit-id: 2b665c149a63865058ea0487d7fb9303d12fb61b

--- scripts/github/workflow.bash
+++ scripts/github/workflow.bash
@@ -3,9 +3,18 @@ set -euo pipefail
 
 # Handles everything involved in GitHub CI processing
 main() {
+    local result
+
     :> logs/aria2.log
+
     ./scripts/v1/build_release.bash
-    find -P -O3 ./build/ -type f -name "*.txt" -exec bash ./scripts/github/update_readme_tag.bash {} \;
+    [[ "$?" = 0 ]] && result='success' || result='failure'
+
+    find -P -O3 ./build/ -type f -name "*.txt" -exec ./scripts/github/update_readme_tag.bash {} \;
+
+    # https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#outputs-for-composite-actions=
+    # https://help.github.com/en/articles/development-tools-for-github-actions#set-an-output-parameter-set-output
+    echo "::set-output name=status::${result}"
 }
 
 main
