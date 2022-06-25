87efa9e0924253775331e959478eb1155821b466
T145 <T145@protonmail.com>
2022-06-25 12:09:35 -0700

    Built the V2 filter infrastructure
    Former-commit-id: 415c7703aa3efb4924e98750ef6ea8dd5e383e17

--- scripts/v2/apply_filters.bash
+++ scripts/v2/apply_filters.bash
@@ -1,15 +1,43 @@
 #!/usr/bin/env bash
 
 main() {
-    local LIST
-    local CACHE
+  local LIST
+  local KEY
+  local METHOD
+  local FILTER
+  local FORMAT
+  local CONTENT_TYPE
+  local CACHE
 
-    LIST="$1"
-    CACHE="$2"
+  LIST="$1"
+  KEY="$2"
+  METHOD="$3"
+  FILTER="$4"
+  FORMAT="$5"
+  CONTENT_TYPE="$6"
+  CACHE="$7"
 
-    readonly LIST CACHE
+  readonly LIST KEY METHOD FILTER FORMAT CONTENT_TYPE CACHE
 
-    echo "[INFO] Operating on: ${LIST}"
+  echo "[INFO] Operating on: ${LIST}"
+
+  cat -s "$LIST" |
+    case "$CONTENT_TYPE" in
+    TEXT)
+      case "$FILTER" in
+      NONE) cat -s ;;
+      esac
+    ;;
+    JSON)
+      case "$FILTER" in
+      esac
+    ;;
+    CSV)
+      case "$FILTER" in
+      esac
+    ;;
+  esac | mawk 'NF && !seen[$0]++'
+  >>"build/${METHOD,,}_${FORMAT,,}.txt"
 }
 
-main "$1" "$2"
+main "$1" "$2" "$3" "$4" "$5" "$6" "$7"
--- scripts/v2/build_lists.bash
+++ scripts/v2/build_lists.bash
@@ -18,41 +18,37 @@ FORMAT_CIDR='CIDR'
 FORMAT_IPV6='IPV6'
 readonly DOWNLOADS METHOD_ALLOW METHOD_BLOCK FORMAT_DOMAIN FORMAT_IPV4 FORMAT_CIDR FORMAT_IPV6
 
-METHODS=("$METHOD_ALLOW" "$METHOD_BLOCK")
+METHODS=("$METHOD_BLOCK" "$METHOD_ALLOW")
 FORMATS=("$FORMAT_DOMAIN" "$FORMAT_IPV4" "$FORMAT_CIDR" "$FORMAT_IPV6")
 readonly -a METHODS
 readonly -a FORMATS
 
 trap 'rm -rf "$DOWNLOADS"' EXIT || exit 1
 
-# params: method
-generate_aria_downloads() {
-    jq -r --arg method "$1" 'to_entries[] |
-        select(.value.method == $method) |
-        {key, mirrors: .value.mirrors} |
-        .ext = (.mirrors[0] | match(".(tar.gz|zip|7z|json)").captures[0].string // "txt") |
-        (.mirrors | join("\t")), " out=\(.key).\(.ext)"' data/v2/lists.json
-}
-
-# params: method, cache
-download_lists() {
-    # 'set +e' temporarily disables strict fail in case downloads fail: the OR clause is to ensure it's enabled after processing.
-    generate_aria_downloads "$1" | (set +e && aria2c -i- -d "$2" --conf-path='./configs/aria2.conf' && set -e) || set -e
-}
-
 main() {
-    local cache
+  local cache
 
-    mkdir -p build/
+  mkdir -p build/
 
-    for method in "${METHODS[@]}"; do
-        cache="${DOWNLOADS}/${method}"
+  for method in "${METHODS[@]}"; do
+    cache="${DOWNLOADS}/${method}"
 
-        download_lists "$method" "$cache"
+    jq -r --arg method "$method" 'to_entries[] |
+      select(.value.method == $method) |
+      {key, mirrors: .value.mirrors} |
+      .ext = (.mirrors[0] | match(".(tar.gz|zip|7z|json)").captures[0].string // "txt") |
+      (.mirrors | join("\t")), " out=\(.key).\(.ext)"' data/v2/lists.json |
+      (set +e && aria2c -i- -d "$cache" --conf-path='./configs/aria2.conf' && set -e) || set -e
 
-        find -P -O3 "$cache" -type f -exec sem -j+0 ./scripts/v2/apply_filters.bash {} "$DOWNLOADS" \;
-        sem --wait
+    jq -r --arg method "$method" 'to_entries[] |
+      select(.value.method == $method) | .key as $k | .value.formats[] |
+      "\($k)#\(.filter)#\(.format)#\(.content.type)"' data/v2/lists.json |
+    while IFS='#' read -r key filter format content_type; do
+      find -P -O3 "$cache" -type f -exec sem -j+0 ./scripts/v2/apply_filters.bash {} "$key" "$method" "$filter" "$format" "$content_type" "$DOWNLOADS" \;
+      sem --wait
+      # else the download failed and src_list is empty
     done
+  done
 }
 
 # https://github.com/koalaman/shellcheck/wiki/SC2218
