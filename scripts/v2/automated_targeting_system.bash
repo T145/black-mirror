e4d65767f42bf3293c7731cc0729767fb6d1d1cd
T145 <T145@protonmail.com>
2022-06-27 00:31:12 -0700

    Write files w/ sponge
    Former-commit-id: 12281df51e1a6b6355d5218e9ae846356d9486f7

--- scripts/v2/automated_targeting_system.bash
+++ scripts/v2/automated_targeting_system.bash
@@ -40,7 +40,7 @@ apply_filter() {
     mawk 'NF && !seen[$0]++' | # Filter blank lines and duplicates!
     #httpx -r configs/resolvers.txt -silent -t 200000 |
     parsort -u -S 100% --parallel=200000 -T "$CACHE" |
-    grep -Fxvf exports/sources.txt -
+    parallel --pipe -k -j100% grep -Fxvf exports/sources.txt -
 }
 
 main() {
@@ -62,11 +62,14 @@ main() {
       fi
     done
 
-  lychee --exclude-mail -nEf Json -o exports/target-status.json -T 200 -t 10 -r 0 -u 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0' target/*.txt 1>/dev/null || true
+  local status
+  status='exports/target-status.json'
 
-  jq -r '.fail_map | to_entries[] | .key as $k | .value[] | select(.status | startswith("Failed:") or startswith("Cached:")) | "\($k)#\(.url)"' exports/target-status.json |
+  lychee --exclude-mail -nEf Json -o "$status" -T 200 -t 10 -r 0 -u 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0' target/*.txt 1>/dev/null || true
+
+  jq -r '.fail_map | to_entries[] | .key as $k | .value[] | select(.status | startswith("Failed:") or startswith("Failed:")) | "\($k)#\(.url)"' "$status" |
     while IFS='#' read -r target url; do
-      echo "$url" | comm "$target" - -23 >"$target"
+      echo "$url" | comm "$target" - -23 | sponge "$target"
     done
 }
 
