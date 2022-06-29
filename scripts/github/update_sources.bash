a9de7d95ec9b00e800960fdc7f2f605c3481ff1e
T145 <T145@protonmail.com>
2022-06-29 01:29:44 -0700

    Fixed V2 source export

--- scripts/github/update_sources.bash
+++ scripts/github/update_sources.bash
@@ -12,16 +12,16 @@ main() {
   case $(dirname "$1" | mawk -F/ '{print $2}') in
   v1)
     jq -jr 'to_entries[] | select(.value.color == "black") | .value.mirrors[0] as $url | .value.filters[] | if (.engine == "cat" or .engine == "mawk") then ($url, " ") else empty end' data/v1/sources.json | sed 's/ $/\n/' >exports/sources.pihole
-    jq -r 'to_entries[] | select(.value.variants != null) | .value.variants[]' "$1" >exports/sources.txt
+    jq -r 'to_entries[] | select(.value.variants != null) | .value.variants[]' "$1" >"$sources"
     ;;
   v2)
     jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.pihole != null) | (.value.variants.pihole, " ")' data/v2/lists.json | sed 's/ $/\n/' >exports/sources.pihole
     jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.adguard != null) | ("!#include ", .value.variants.adguard, "\n")' data/v2/lists.json >exports/sources.adguard
-    jq -r 'to_entries[] | select(.value.variants != null) | .value.variants | to_entries[].url' "$1" >exports/sources.txt
+    jq -r 'to_entries[].value | select(.variants != null) | .variants | to_entries[].value' "$1" >"$sources"
     ;;
   esac
 
-  jq -r 'to_entries[].value.mirrors[]' "$1" >>exports/sources.txt
+  jq -r 'to_entries[].value.mirrors[]' "$1" >>"$sources"
   sort -bfiuS 100% -o "$sources" --parallel 200000 "$sources"
 }
 
