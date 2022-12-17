3447dadcebb5a826077c66629a34de93916578f4
T145 <T145@protonmail.com>
2022-12-17 13:30:00 -0500

    feat(v1): Set ATS to target V1; the active version

--- scripts/ats/automated_targeting_system.bash
+++ scripts/ats/automated_targeting_system.bash
@@ -9,8 +9,10 @@ set -euET -o pipefail # put bash into strict mode & have it give descriptive err
 umask 055             # change all generated file perms from 755 to 700
 export LC_ALL=C       # force byte-wise sorting and default langauge output
 
+SOURCES='/data/v1/targets.json'
+STATUS='/dist/ats/target-status.json'
 CACHE=$(mktemp -d)
-readonly CACHE
+readonly SOURCES STATUS CACHE
 
 trap 'rm -rf "$CACHE"' EXIT || exit 1
 
@@ -36,7 +38,7 @@ apply_filter() {
 	esac |
 		# Format github.com/*/raw/* and rawcdn.githack.com URLs as raw.githubusercontent.com, b/c they aren't technically mirrors and redirect to raw.githubusercontent.com.
 		# Any github.com/*/archive/* URLs are ignored, since single lists are used over an entire repository.
-		./scripts/v2/raw_github_url_format.awk |
+		./scripts/ats/raw_github_url_format.awk |
 		mawk 'NF && !seen[$0]++' | # Filter blank lines and duplicates!
 		#httpx -r configs/resolvers.txt -silent -t 200000 |
 		parsort -bfiu -S 100% --parallel=200000 -T "$CACHE" |
@@ -45,14 +47,14 @@ apply_filter() {
 
 main() {
 	git config --global --add safe.directory /__w/black-mirror/black-mirror
-	jq -SM '.' data/v2/targets.json | sponge data/v2/targets.json
+	jq -SM '.' "$SOURCES" | sponge "$SOURCES"
 
-	jq -r 'to_entries[] | (.value.mirror), " out=\(.key).txt"' data/v2/targets.json |
+	jq -r 'to_entries[] | (.value.mirror), " out=\(.key).txt"' "$SOURCES" |
 		(set +e && aria2c -i- -d "$CACHE" --conf-path='./configs/aria2.conf' && set -e) || set -e
 
 	local list
 
-	jq -r 'to_entries[] | "\(.key)#\(.value.content.filter)"' data/v2/targets.json |
+	jq -r 'to_entries[] | "\(.key)#\(.value.content.filter)"' "$SOURCES" |
 		while IFS='#' read -r key filter; do
 			list="${CACHE}/${key}.txt"
 
@@ -61,13 +63,10 @@ main() {
 			fi
 		done
 
-	local status
-	status='dist/ats/target-status.json'
+	lychee --exclude-mail -nEf Json -o "$STATUS" -T 200 -t 10 -r 0 -u 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0' dist/ats/*.txt 1>/dev/null || true
+	jq -SM '.' "$STATUS" | sponge "$STATUS"
 
-	lychee --exclude-mail -nEf Json -o "$status" -T 200 -t 10 -r 0 -u 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0' dist/ats/*.txt 1>/dev/null || true
-	jq -SM '.' "$status" | sponge "$status"
-
-	jq -r '.fail_map | to_entries[] | .key as $k | .value[] | select(.status | startswith("Failed:") or startswith("Cached:")) | "\($k)#\(.url)"' "$status" |
+	jq -r '.fail_map | to_entries[] | .key as $k | .value[] | select(.status | startswith("Failed:") or startswith("Cached:")) | "\($k)#\(.url)"' "$STATUS" |
 		while IFS='#' read -r target url; do
 			echo "$url" | grep -Fxvf - "$target" | sponge "$target"
 		done
