1d61ff7329334a4bd5679e1460b03966448ea5ef
T145 <T145@protonmail.com>
2023-04-02 12:38:19 -0700

    fix(actions): Fixed setting GitHub output

--- scripts/github/workflow.bash
+++ scripts/github/workflow.bash
@@ -10,7 +10,8 @@ main() {
 
 		# https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#outputs-for-composite-actions=
 		# https://help.github.com/en/articles/development-tools-for-github-actions#set-an-output-parameter-set-output
-		echo "::set-output name=status::success"
+		# https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
+		echo "status=success" >> "$GITHUB_OUTPUT"
 	else
 		cat <&2
 		exit 1
