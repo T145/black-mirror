070c7c956a69456488910403fac8fc956686437b
T145 <T145@protonmail.com>
2023-04-06 05:20:45 -0700

    fix(v2): Repaired multithreading

--- scripts/v2/apply_filters.bash
+++ scripts/v2/apply_filters.bash
@@ -1,112 +1,73 @@
 #!/usr/bin/env bash
 
-# params: list, content filter
-apply_content_filter() {
-    case $2 in
-    NONE) cat -s "$1" ;;
-    7Z) 7za -y -so e "$1" ;;
-    ZIP) zcat "$1" ;;
-    SQUIDGUARD) tar -xOzf "$1" --wildcards-match-slash --wildcards '*/domains' ;;
-    esac
-}
-
 get_domains_from_urls() {
-    perl -M'Data::Validate::Domain qw(is_domain)' -MRegexp::Common=URI -nE 'while (/$RE{URI}{HTTP}{-scheme => "https?"}{-keep}/g) {say $3 if is_domain($3)}'
+    perl -M'Data::Validate::Domain qw(is_domain)' -MRegexp::Common=URI -nE 'while (/$RE{URI}{HTTP}{-scheme => "https?"}{-keep}/g) {say $3 if is_domain($3)}' 2>/dev/null
 }
 
 get_ipv4s_from_urls() {
-    perl -M'Data::Validate::IP qw(is_ipv4)' -MRegexp::Common=URI -nE 'while (/$RE{URI}{HTTP}{-scheme => "https?"}{-keep}/g) {say $3 if is_ipv4($3)}'
-}
-
-# params: content type, format filter
-# REQUIRED OUTPUT: Unverified hosts applicable to the designated format (no empty lines)
-apply_format_filter() {
-    case $1 in
-    TEXT)
-        case $2 in
-        NONE) cat -s ;;
-        RAW_HOSTS_WITH_COMMENTS) mawk '/^[^[:space:]|^#|^!]/{print $1}' ;;
-        HOSTS_FILE) ghosts -m /dev/fd/0 -o -p -noheader -stats=false ;;
-        ABUSE_CH_URLHAUS_DOMAIN) get_domains_from_urls ;;
-        ABUSE_CH_URLHAUS_IPV4) get_ipv4s_from_urls ;;
-        esac
-        ;;
-    JSON)
-        case $2 in
-        ABUSE_CH_FEODOTRACKER_IPV4) jq -r '.[].ip_address' ;;
-        ABUSE_CH_FEODOTRACKER_DOMAIN) jq -r '.[] | select(.hostname != null) | .hostname' ;;
-        ABUSE_CH_THREATFOX_IPV4) jq -r 'to_entries[].value[].ioc_value | split(":")[0]' ;;
-        ABUSE_CH_THREATFOX_DOMAIN) jq -r 'to_entries[].value[].ioc_value' ;;
-        esac
-        ;;
-    CSV)
-        case $2 in
-        MLR_CUT_1) mlr --mmap --csv --skip-comments -N cut -f 1 ;;
-        MLR_CUT_2) mlr --mmap --csv --skip-comments -N cut -f 2 ;;
-        MLR_CUT_3) mlr --mmap --csv --skip-comments -N cut -f 3 ;;
-        MLR_CUT_4) mlr --mmap --csv --skip-comments -N cut -f 4 ;;
-        esac
-        ;;
-    esac | # filter blank lines and duplicates
-        mawk 'NF && !seen[$0]++'
-}
-
-# params: list format, method, key
-validate_cidr() {
-    # https://metacpan.org/pod/Net::CIDR#$ip=Net::CIDR::cidrvalidate($ip);
-    perl -MNet::CIDR=cidrvalidate -nE 'chomp($_); if(defined($_) && index($_, "/") != -1 && cidrvalidate($_)) {say $_;}' |
-        ipinfo prips |
-        validate_output "$1" "$2" "$3"
-}
-
-# params: list format, method, key
-validate_output() {
-    case $1 in
-    DOMAIN)
-        perl ./scripts/v1/process_domains.pl 2>/dev/null
-            >>"build/${2}_${1}.txt"
-        ;;
-    IPV4)
-        perl -MData::Validate::IP -nE 'chomp($_); if(defined($_) && is_ipv4($_) && !is_unroutable_ipv4($_) && !is_private_ipv4($_) && !is_loopback_ipv4($_) && !is_linklocal_ipv4($_) && !is_testnet_ipv4($_)) {say $_;}'
-            >>"build/${2}_${1}.txt"
-        ;;
-    IPV6)
-        perl -MData::Validate::IP -nE 'chomp($_); if(defined($_) && is_ipv6($_)) {say $_;}'
-            >>"build/${2}_${1}.txt"
-        ;;
-    CIDR4) validate_cidr 'IPV4' "$2" "$3" ;;
-    CIDR6) validate_cidr 'IPV6' "$2" "$3" ;;
-    *)
-        echo "[ERROR] Invalid ${1} format from ${3}!"
-        exit 1
-        ;;
-    esac
+    perl -M'Data::Validate::IP qw(is_ipv4)' -MRegexp::Common=URI -nE 'while (/$RE{URI}{HTTP}{-scheme => "https?"}{-keep}/g) {say $3 if is_ipv4($3)}' 2>/dev/null
 }
 
 main() {
-    local LIST
-    local KEY
+    local FILE_PATH
+    local LIST_NAME
+    local LIST_METHOD
     local CONTENT_FILTER
     local CONTENT_TYPE
-    local METHOD
     local LIST_FILTER
     local LIST_FORMAT
 
-    LIST="$1"
-    KEY="$2"
+    FILE_PATH="$1"
+    LIST_METHOD="$2"
     CONTENT_FILTER="$3"
     CONTENT_TYPE="$4"
-    METHOD="$5"
-    LIST_FILTER="$6"
-    LIST_FORMAT="$7"
+    LIST_FILTER="$5"
+    LIST_FORMAT="$6"
 
-    readonly LIST KEY CONTENT_FILTER METHOD LIST_FILTER LIST_FORMAT
-
-    #echo "[INFO] Operating on ${LIST} with ${LIST_FORMAT} content with the ${CONTENT_FILTER} filter."
-
-    apply_content_filter "$LIST" "$CONTENT_FILTER" |
-        apply_format_filter "$CONTENT_TYPE" "$LIST_FILTER" |
-        validate_output "$LIST_FORMAT" "$METHOD" "$KEY"
+    case "$CONTENT_FILTER" in
+    'NONE') cat -s "$FILE_PATH" ;;
+    '7Z') 7za -y -so e "$FILE_PATH" ;;
+    'ZIP') zcat "$FILE_PATH" ;;
+    'SQUIDGUARD') tar -xOzf "$FILE_PATH" --wildcards-match-slash --wildcards '*/domains' ;;
+    esac |
+        case "$CONTENT_TYPE" in
+        'TEXT')
+            case "$LIST_FILTER" in
+            'NONE') cat -s ;;
+            'RAW_HOSTS_WITH_COMMENTS') mawk '/^[^[:space:]|^#|^!]/{print $1}' ;;
+            'HOSTS_FILE') ghosts -m /dev/stdin -o -p -noheader -stats=false ;;
+            'ABUSE_CH_URLHAUS_DOMAIN') get_domains_from_urls ;;
+            'ABUSE_CH_URLHAUS_IPV4') get_ipv4s_from_urls ;;
+            esac
+            ;;
+        'JSON')
+            case "$LIST_FILTER" in
+            'ABUSE_CH_FEODOTRACKER_IPV4') jq -r '.[].ip_address' ;;
+            'ABUSE_CH_FEODOTRACKER_DOMAIN') jq -r '.[] | select(.hostname != null) | .hostname' ;;
+            'ABUSE_CH_THREATFOX_IPV4') jq -r 'to_entries[].value[].ioc_value | split(":")[0]' ;;
+            'ABUSE_CH_THREATFOX_DOMAIN') jq -r 'to_entries[].value[].ioc_value' ;;
+            esac
+            ;;
+        'CSV')
+            case "$LIST_FILTER" in
+            'MLR_CUT_1') GODEBUG=gctrace=1 mlr --csv --skip-comments -N clean-whitespace then cut -f 1 ;;
+            'MLR_CUT_2') GODEBUG=gctrace=1 mlr --csv --skip-comments -N clean-whitespace then cut -f 2 ;;
+            'MLR_CUT_3') GODEBUG=gctrace=1 mlr --csv --skip-comments -N clean-whitespace then cut -f 3 ;;
+            'MLR_CUT_4') GODEBUG=gctrace=1 mlr --csv --skip-comments -N clean-whitespace then cut -f 4 ;;
+            esac
+            ;;
+        esac | mawk 'NF && !seen[$0]++' |
+            case "$LIST_FORMAT" in
+            'DOMAIN')
+                perl ./scripts/v1/process_domains.pl 2>/dev/null
+                ;;
+            'IPV4')
+                perl -M'Data::Validate::IP' -nE 'chomp($_); if(defined($_) && is_ipv4($_) && !is_unroutable_ipv4($_) && !is_private_ipv4($_) && !is_loopback_ipv4($_) && !is_linklocal_ipv4($_) && !is_testnet_ipv4($_)) {say $_;}' 2>/dev/null
+                ;;
+            'IPV6')
+                perl -M'Data::Validate::IP' -nE 'chomp($_); if(defined($_) && is_ipv6($_)) {say $_;}' 2>/dev/null
+                ;;
+            esac >>"build/${LIST_METHOD}_${LIST_FORMAT}.txt"
 }
 
-main "$1" "$2" "$3" "$4" "$5" "$6" "$7"
+main "$1" "$2" "$3" "$4" "$5" "$6"
--- scripts/v2/build_lists.bash
+++ scripts/v2/build_lists.bash
@@ -25,25 +25,10 @@ FORMATS=("$FORMAT_DOMAIN" "$FORMAT_CIDR4" "$FORMAT_CIDR6" "$FORMAT_IPV4" "$FORMA
 readonly -a METHODS
 readonly -a FORMATS
 
-# params: file path
-sorted() {
-	parsort -bfiu -S 100% -T "$DOWNLOADS" "$1" | sponge "$1"
-}
-
-# merge list 2 into list 1
-# params: list 1, list 2
-merge_lists() {
-	cat "$1" "$2" | sponge "$1"
-	sorted "$1"
-}
-
 main() {
 	trap 'rm -rf "$DOWNLOADS"' EXIT || exit 1
 
 	local cache
-	local list
-	local nxlist
-	local blacklist
 
 	mkdir -p build/
 	: >logs/aria2.log
@@ -57,9 +42,7 @@ main() {
 		jq -r --arg method "$method" 'to_entries[] |
 			select(.value.content.retriever == "ARIA2" and .value.method == $method) |
 			{key, mirrors: .value.mirrors} |
-			.ext = (.mirrors[0] |
-			match(".(tar.gz|zip|7z|json)").captures[0].string // "txt") |
-			(.mirrors | join("\t")), " out=\(.key).\(.ext)"' data/v2/lists.json |
+			(.mirrors | join("\t")), " out=\(.key)"' data/v2/lists.json |
 			aria2c -i- -d "$cache" --conf-path='./configs/aria2.conf'
 		set -e
 
@@ -71,52 +54,10 @@ main() {
 			.value.formats[] |
 			"\($key)#\($content_filter)#\($content_type)#\(.filter)#\(.format)"' data/v2/lists.json |
 				while IFS='#' read -r key content_filter content_type list_filter list_format; do
-					find -P -O3 "$cache" -type f -exec sem -j+0 ./scripts/v2/apply_filters.bash {} "$key" "$content_filter" "$content_type" "$method" "$list_filter" "$list_format" \; 1>/dev/null
+					find -P -O3 "$cache" -name "$key" -type f -exec sem -j+0 ./scripts/v2/apply_filters.bash {} "$method" "$content_filter" "$content_type" "$list_filter" "$list_format" \;
 				done
 
 		sem --wait
-
-		# for format in "${FORMATS[@]}"; do
-		# 	list="build/${method}_${format}.txt"
-		# 	nxlist="dist/NX${format}.txt"
-
-		# 	if test -f "$list"; then
-		# 		if [[ "$format" != "$FORMAT_CIDR4" || "$format" != "$FORMAT_CIDR6" ]]; then
-		# 			if [[ "$method" == "$METHOD_BLOCK" ]]; then
-		# 				# if the nxlist is present, then rescan it to see if any hosts are online
-		# 				# put any online hosts into the blacklist and remove them from the nxlist
-		# 				# rescan the blacklist using the nxlist as a hosts file to optimize searching
-		# 				if test -f "$nxlist"; then
-		# 					# TODO: Export JSON from dnsX and use jq to pull out domains & ips
-		# 					dnsx -r ./configs/resolvers.txt -l "$nxlist" -o "$TMP" -c 200000 -silent -rcode noerror,servfail,refused 1>/dev/null
-		# 					# remove online hosts from the nxlist
-		# 					grep -Fxvf "$TMP" "$nxlist" | sponge "$nxlist"
-		# 					dnsx -r ./configs/resolvers.txt -hf "$nxlist" -l "$list" -o "$nxlist" -c 200000 -silent -rcode nxdomain 1>/dev/null
-		# 					merge_lists "$list" "$TMP"
-		# 					#comm "$nxlist" "$TMP" -23 | sponge "$nxlist"
-		# 					: >"$TMP"
-		# 				else
-		# 					sorted "$list"
-		# 					dnsx -r ./configs/resolvers.txt -l "$list" -o "$nxlist" -c 200000 -silent -rcode nxdomain 1>/dev/null
-		# 				fi
-
-		# 				sorted "$nxlist"
-		# 			else
-		# 				# apply the whitelist to the blacklist
-		# 				blacklist="build/BLOCK_${format}.txt"
-
-		# 				# merge the nxlist and whitelist
-		# 				merge_lists "$list" "$nxlist"
-
-		# 				# https://askubuntu.com/a/562352
-		# 				# send each line into the temp file as it's processed instead of keeping it in memory
-		# 				parallel --pipe -k -j+0 grep --line-buffered -Fxvf "$list" - <"$blacklist" >>"$TMP"
-		# 				cp "$TMP" "$blacklist"
-		# 				: >"$TMP"
-		# 			fi
-		# 		fi
-		# 	fi
-		# done
 	done
 }
 
