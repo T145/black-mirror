1dab01b261116e13283801d599c46bb3725b79c0
T145 <T145@protonmail.com>
2023-04-12 02:19:01 -0700

    fix(sources): Fixed source updates

--- scripts/github/update_sources.bash
+++ scripts/github/update_sources.bash
@@ -2,26 +2,12 @@
 
 # Sorts the designated source file and constructs new list formats from it
 main() {
-	local sources
-
-	sources='dist/sources.txt'
-
 	git config --global --add safe.directory /__w/black-mirror/black-mirror
-
-	case $(dirname "$1" | mawk -F/ '{print $2}') in
-	v1)
-		jq -jr 'to_entries[] | select(.value.color == "black") | .value.mirrors[0] as $url | .value.filters[] | if (.engine == "cat" or .engine == "mawk") then ($url, " ") else empty end' data/v1/sources.json | sed 's/ $/\n/' >dist/sources.pihole
-		jq -r 'to_entries[] | select(.value.variants != null) | .value.variants[]' "$1" >"$sources"
-		;;
-	v2)
-		jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.pihole != null) | (.value.variants.pihole, " ")' data/v2/lists.json | sed 's/ $/\n/' >dist/sources.pihole
-		jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.adguard != null) | ("!#include ", .value.variants.adguard, "\n")' data/v2/lists.json >dist/sources.adguard
-		jq -r 'to_entries[].value | select(.variants != null) | .variants | to_entries[].value' "$1" >"$sources"
-		;;
-	esac
-
-	jq -r 'to_entries[].value.mirrors[]' "$1" >>"$sources"
-	sort -bfiuS 100% -o "$sources" --parallel 200000 "$sources"
+	jq -Sr '.' -f data/v2/lists.json >data/v2/lists.json
+	jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.pihole != null) | (.value.variants.pihole, " ")' data/v2/lists.json | sed 's/ $/\n/' >dist/sources.pihole
+	jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.adguard != null) | ("!#include ", .value.variants.adguard, "\n")' data/v2/lists.json >dist/sources.adguard
+	jq -r 'to_entries[].value | select(.variants != null) | .variants | to_entries[].value' "$1" >"$sources"
+	jq -r 'to_entries[].value.mirrors[]' "$1" >>dist/sources.txt
 }
 
-main "$1"
+main
