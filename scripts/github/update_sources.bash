08b55c44a2edecb6b89beeef53786f4fb4745f68
T145 <T145@protonmail.com>
2023-04-12 02:22:02 -0700

    fix(sources): For real this time

--- scripts/github/update_sources.bash
+++ scripts/github/update_sources.bash
@@ -2,12 +2,16 @@
 
 # Sorts the designated source file and constructs new list formats from it
 main() {
+	local lists
+
+	lists='data/v2/lists.json'
+
 	git config --global --add safe.directory /__w/black-mirror/black-mirror
-	jq -Sr '.' -f data/v2/lists.json >data/v2/lists.json
-	jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.pihole != null) | (.value.variants.pihole, " ")' data/v2/lists.json | sed 's/ $/\n/' >dist/sources.pihole
-	jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.adguard != null) | ("!#include ", .value.variants.adguard, "\n")' data/v2/lists.json >dist/sources.adguard
-	jq -r 'to_entries[].value | select(.variants != null) | .variants | to_entries[].value' "$1" >"$sources"
-	jq -r 'to_entries[].value.mirrors[]' "$1" >>dist/sources.txt
+	jq -Sr '.' -f "$lists" >"$lists"
+	jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.pihole != null) | (.value.variants.pihole, " ")' "$lists" | sed 's/ $/\n/' >dist/sources.pihole
+	jq -jr 'to_entries[] | select(.value.method == "BLOCK" and .value.variants.adguard != null) | ("!#include ", .value.variants.adguard, "\n")' "$lists" >dist/sources.adguard
+	jq -r 'to_entries[].value | select(.variants != null) | .variants | to_entries[].value' "$lists" >"$sources"
+	jq -r 'to_entries[].value.mirrors[]' "$lists" >>dist/sources.txt
 }
 
 main
