bf8eaa2b370a9ba1c34e4736f35dcc82b16405db
T145 <T145@protonmail.com>
2024-04-30 17:37:50 -0400

    feat(lists): Updated URLhaus sources to CSV mirrors

--- scripts/v2/apply_filters.bash
+++ scripts/v2/apply_filters.bash
@@ -8,18 +8,6 @@ get_ipv6s() {
 	ipinfo grepip -6hox --nocolor
 }
 
-get_ipv4_cidrs() {
-	ipinfo grepip -4h --nocolor --cidrs-only
-}
-
-get_ipv6_cidrs() {
-	ipinfo grepip -6h --nocolor --cidrs-only
-}
-
-get_domains() {
-	perl -MData::Validate::IP=is_ipv4 -nE 'chomp; if(defined($_) && !is_ipv4($_)) {say $_;}'
-}
-
 get_domains_from_urls() {
 	perl -MData::Validate::Domain=is_domain -MRegexp::Common=URI -nE 'while (/$RE{URI}{HTTP}{-scheme => "https?|udp"}{-keep}/g) {say $3 if is_domain($3, { domain_private_tld => { onion => 1 } })}' 2>/dev/null
 }
@@ -72,8 +60,8 @@ process_list() {
 			'MIXED_HOSTS_FILE') mawk '{if($1~/^(0.0.0.0)/){print $2}else{if($1~/^[^[:space:]|^#]/&&$1!~/\*$/){print $1}}}' ;;
 			'NPC_HOSTS') mawk '$1~/^0.0.0.0/{for (i=2; i<=NF; i++) print $i}' ;;
 			'RAW_HOSTS_WITH_COMMENTS') mawk '/^[^[:space:]|^#|^!|^;|^$|^:|^*]/{print $1}' ;;
-			'ABUSE_CH_URLHAUS_DOMAIN') get_domains_from_urls ;;
-			'ABUSE_CH_URLHAUS_IPV4') get_ipv4s_from_urls ;;
+			#'ABUSE_CH_URLHAUS_DOMAIN') get_domains_from_urls ;;
+			#'ABUSE_CH_URLHAUS_IPV4') get_ipv4s_from_urls ;;
 			'ALIENVAULT') mawk -F# '{print $1}' ;;
 			'ADBLOCK') hostsblock ;;
 			'GREP_IPV4') get_ipv4s ;;
@@ -98,8 +86,6 @@ process_list() {
 			'ZEEK_DOMAIN') mawk '/^[^[:space:]|^#]/&&$2~/^Intel::DOMAIN$/{print $1}' ;;
 			'ZEEK_IPV4') mawk '/^[^[:space:]|^#]/&&$2~/^Intel::ADDR$/{print $1}' ;;
 			'BETTER_FYI') gawk 'BEGIN{FS="[|^]"}/^\|\|([[:alnum:]_-]{1,63}\.)+[[:alpha:]]+(\$third-party)?$/{print tolower($3)}' | mawk -F$ '{print $1}' ;;
-			#'HERRBISCHOFF_IPV4') mawk '/./&&$0!~/\/|:|^#/' ;;
-			#'HERRBISCHOFF_IPV6') mawk '/:/&&$0!~/\/|^#/' ;;
 			'HERRBISCHOFF_CIDR4') mawk '/\//&&$0!~/:/' ;;
 			'HERRBISCHOFF_CIDR6') mawk '/:/&&/\//&&$0!~/^#/' ;;
 			'POP3GROPERS_IPV4') mawk '/./&&$0!~/\/|:|^#/{gsub(/ /, "", $1); print $1}' ;;
@@ -124,10 +110,7 @@ process_list() {
 			'CYBER_CURE_IPV4') jaq -r '.data.ip[]' ;;
 			'CYBER_CURE_DOMAIN_URL') jaq -r '.data.urls[] | select(test("https?://(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))") | not) | capture("^((?<scheme>[^:/?#]+):)?(//(?<authority>(?<domain>[^/?#:]*)(:(?<port>[0-9]*))?))?").domain' ;;
 			'CYBER_CURE_IP_URL') jaq -r '.data.urls[] | select(test("https?://(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))")) | capture("^((?<scheme>[^:/?#]+):)?(//(?<authority>(?<ip>[^/?#:]*)(:(?<port>[0-9]*))?))?").ip' ;;
-			#'CYBERSAIYAN_DOMAIN') jaq -r '.[] | select(.value.type == "URL") | .indicator' | get_domains_from_urls ;;
-			#'CYBERSAIYAN_IPV4') jaq -r '.[] | select(.value.type == "URL") | .indicator' | get_ipv4s_from_urls ;;
 			'DISCONNECTME_ENTITIES') jaq -r '.entities[] | "\(.properties[])\n\(.resources[])"' ;;
-			#'DISCONNECTME_SERVICES') jaq -r '.categories[] | to_entries[].value[] | to_entries[].value[]' ;;
 			'HIPO_UNIVERSITIES') jaq -r '.[].domains | join("\n")' ;;
 			'ISCSANS') jaq -r '.[].ipv4' ;;
 			'MALSILO_DOMAIN') jaq -r '.data[].network_traffic | select(.dns != null) | .dns[]' ;;
@@ -140,7 +123,6 @@ process_list() {
 			'CHONG_LUA_DAO') jaq -r '.[].url | sub("\\*\\.";"") | capture("^((?<scheme>[^:/?#]+):)?(//(?<authority>(?<domain>[^/?#:]*)(:(?<port>[0-9]*))?))?").domain' ;;
 			'INQUEST_DOMAIN') jaq -r '.data[] | select(.artifact_type == "domain") | .artifact' ;;
 			'INQUEST_IPV4') jaq -r '.data[] | select(.artifact_type == "ipaddress") | .artifact' ;;
-			#'CERTEGO') jaq -rs '.[].links[].url' | mawk -F/ '$5~/^domain$/{print $6}' ;;
 			'SECUREDROP') jaq -r '.[] | .onion_address as $onion | .organization_url | split("/")[2] as $org | $org, $onion' ;;
 			'VIVALDI') jaq -r '.[] | select(.filterStatus == "ON") | .reviewedSite' ;;
 			'MSEDGE') jaq -r '.sites[].url' ;;
@@ -160,26 +142,28 @@ process_list() {
 			'MLR_CUT_1') mlr_cut_col 1 ;;
 			'MLR_CUT_2') mlr_cut_col 2 ;;
 			'MLR_CUT_4') mlr_cut_col 4 ;;
-			'BENKOW_DOMAIN') mlr --mmap --csv --headerless-csv-output --ifs ';' put -S '$url =~ "https?://((([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3}))"; $Domain = "\1"' then cut -f Domain then uniq -a ;;
-			'BENKOW_IPV4') mlr --mmap --csv --headerless-csv-output --ifs ';' put -S '$ip =~ "(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))"; $IP = "\1"' then cut -f IP then uniq -a ;;
+			'ABUSE_CH_URLHAUS_ONLINE_DOMAIN') mlr --mmap --csv --headerless-csv-output --skip-comments -N put -S '$3 =~ "https?://([a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,6})"; $Domain = "\1"' then cut -f Domain ;;
+			'ABUSE_CH_URLHAUS_ONLINE_IPV4') mlr --mmap --csv --headerless-csv-output --skip-comments -N put -S '$3 =~ "(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))"; $IP = "\1"' then cut -f IP ;;
+			'BENKOW_DOMAIN') mlr --mmap --csv --headerless-csv-output --ifs ';' put -S '$url =~ "https?://([a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,6})"; $Domain = "\1"' then cut -f Domain ;;
+			'BENKOW_IPV4') mlr --mmap --csv --headerless-csv-output --ifs ';' put -S '$ip =~ "(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))"; $IP = "\1"' then cut -f IP ;;
 			'BOTVIRJ_COVID') mawk 'NR>1' ;;
 			'MALWARE_DISCOVERER_DOMAIN') mlr --mmap --csv --headerless-csv-output cut -f domain ;;
 			'MALWARE_DISCOVERER_IPV4') mlr --mmap --csv --headerless-csv-output cut -f ip ;;
-			'PHISHSTATS_DOMAIN') mlr --mmap --csv --skip-comments -N put -S '$3 =~ "https?://((([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3}))"; $Domain = "\1"' then cut -f Domain then uniq -a phish_score.csv ;;
-			'PHISHSTATS_IPV4') mlr --mmap --csv --skip-comments -N put -S '$4 =~ "(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))"; $IP = "\1"' then cut -f IP then uniq -a ;;
+			'PHISHSTATS_DOMAIN') mlr --mmap --csv --skip-comments -N put -S '$3 =~ "https?://([a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,6})"; $Domain = "\1"' then cut -f Domain ;;
+			'PHISHSTATS_IPV4') mlr --mmap --csv --skip-comments -N put -S '$4 =~ "(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))"; $IP = "\1"' then cut -f IP ;;
 			# https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
 			# IpInfo uses the same regex, so there's less overhead by processing everything in Miller.
-			'PHISHSTATS_IPV6') mlr --mmap --csv --skip-comments -N put -S '$4 =~ "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"; $IP = "\1"' then cut -f IP then uniq -a ;;
+			'PHISHSTATS_IPV6') mlr --mmap --csv --skip-comments -N put -S '$4 =~ "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"; $IP = "\1"' then cut -f IP ;;
 			'TURRIS') mlr --mmap --csv --headerless-csv-output --skip-comments cut -f Address ;;
-			'VIRIBACK_DOMAIN') mlr --mmap --csv --headerless-csv-output put -S '$URL =~ "https?://((([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3}))"; $Domain = "\1"' then cut -f Domain then uniq -a ;;
-			'VIRIBACK_IPV4') mlr --mmap --csv --headerless-csv-output cut -f IP then uniq -a ;;
+			'VIRIBACK_DOMAIN') mlr --mmap --csv --headerless-csv-output put -S '$URL =~ "https?://([a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,6})"; $Domain = "\1"' then cut -f Domain ;;
+			'VIRIBACK_IPV4') mlr --mmap --csv --headerless-csv-output cut -f IP ;;
 			'SHADOWSERVER_HOST') mlr --mmap --csv --headerless-csv-output cut -f http_host ;;
 			'SHADOWSERVER_TARGET') mlr --mmap --csv --headerless-csv-output cut -f redirect_target ;;
 			'WATCHLIST_INTERNET') mlr --mmap --csv --ifs ';' -N cut -f 1 ;;
 			'CRUZ_IT') mlr --mmap --csv --headerless-csv-output clean-whitespace then cut -f ip_address ;;
 			# PhishTank often includes large hashes in its URLs, which may lead to parsing issues.
 			# Therefore use regex that ignores paths and queries after the domain.
-			'PHISHTANK') mlr --mmap --csv --headerless-csv-output --lazy-quotes put -S '$url =~ "https?://([^/]+)"; $Domain = "\1"' then cut -f Domain then uniq -a ;;
+			'PHISHTANK') mlr --mmap --csv --headerless-csv-output --lazy-quotes put -S '$url =~ "https?://([^/]+)"; $Domain = "\1"' then cut -f Domain ;;
 			'BLOCKLIST_UA') mlr --mmap --csv --headerless-csv-output --ifs ';' cut -f IP ;;
 			# The C2 feed has malformed CSVs.
 			'THREATVIEW_C2_HOSTS') mawk -F, '/^[^#]/{print $3}' ;;
@@ -194,6 +178,7 @@ process_list() {
 			esac
 			;;
 		esac |
+			# Remove empty lines and duplicates
 			mawk 'NF && !seen[$0]++' |
 			case "$LIST_FORMAT" in
 			'DOMAIN')
